a54bac22e4ef22401650a308ef31cc142a6a2fd4
2e12 <gabriel.beutler@4teamwork.ch>
2020-04-30 14:55:59 +0200

    Setup base buefy app.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <b-navbar class="is-primary">
+            <template slot="start">
+                <b-navbar-item tag="router-link" to="/">
+                    Home
+                </b-navbar-item>
+
+                <b-navbar-item tag="router-link" to="/about">
+                    About
+                </b-navbar-item>
+            </template>
+        </b-navbar>
+        <section class="section">
+            <div class="container">
+                <router-view/>
+            </div>
+        </section>
     </div>
-    <router-view/>
-  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
